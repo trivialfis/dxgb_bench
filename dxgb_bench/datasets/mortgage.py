78302735bc70af94a48b9973c92582d55feeb57f
TrellixVulnTeam <kasimir.schulz@trellix.com>
2022-10-02 13:27:03 +0000

    Adding tarfile member sanitization to extractall()

--- dxgb_bench/datasets/mortgage.py
+++ dxgb_bench/datasets/mortgage.py
@@ -227,7 +227,26 @@ class Mortgage(DataSet):
         if not extracted:
             with tarfile.open(filename, "r:gz") as tarball:
                 fprint("Extracting", filename)
-                tarball.extractall(self.local_directory)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner) 
+                    
+                
+                safe_extract(tarball, self.local_directory)
 
         self.task = "binary:logistic"
 
