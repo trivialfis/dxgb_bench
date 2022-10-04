8a3eb56a5f967931dc0d2834acdd881dcf0eacac
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 18:12:04 +0000

    Adding numeric_owner as keyword arguement

--- dxgb_bench/datasets/mortgage.py
+++ dxgb_bench/datasets/mortgage.py
@@ -243,7 +243,7 @@ class Mortgage(DataSet):
                         if not is_within_directory(path, member_path):
                             raise Exception("Attempted Path Traversal in Tar File")
                 
-                    tar.extractall(path, members, numeric_owner) 
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
                     
                 
                 safe_extract(tarball, self.local_directory)
