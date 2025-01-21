48756c7f97e73a9f106b93736d0ae098bccb7f85
Jiaming Yuan <jm.yuan@outlook.com>
2025-01-21 16:22:32 +0800

    revert.

--- tests/test_datagen.sh
+++ tests/test_datagen.sh
@@ -5,12 +5,12 @@ DATADIRS="./_tdata0,./_tdata1"
 echo "Run datagen"
 dxgb-bench datagen --n_samples_per_batch=512 --n_batches=8 --n_features=256 --saveto=${DATADIRS}
 
-# echo "Run bench"
-# dxgb-bench bench --device=cpu --task=qdm --valid --loadfrom=${DATADIRS}
-# dxgb-bench bench --device=cuda --task=qdm --valid --loadfrom=${DATADIRS}
+echo "Run bench"
+dxgb-bench bench --device=cpu --task=qdm --valid --loadfrom=${DATADIRS}
+dxgb-bench bench --device=cuda --task=qdm --valid --loadfrom=${DATADIRS}
 
 echo "Run extmem bench with QDM"
-# dxgb-ext-bench --task=ext-qdm --device=cpu --valid --loadfrom=${DATADIRS}
+dxgb-ext-bench --task=ext-qdm --device=cpu --valid --loadfrom=${DATADIRS}
 dxgb-ext-bench --task=ext-qdm --device=cuda --valid --loadfrom=${DATADIRS}
 
 echo "Remove data"
