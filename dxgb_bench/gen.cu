8d7dc02f19c158955e75510e1c257994decbfe4f
Jiaming Yuan <jm.yuan@outlook.com>
2025-01-26 16:46:03 +0800

    More.

--- dxgb_bench/gen.cu
+++ dxgb_bench/gen.cu
@@ -50,7 +50,7 @@ int MakeDenseRegression(bool is_cuda, int64_t m, int64_t n, double sparsity, int
     Impl(thrust::cuda::par_nosync, m, n, sparsity, seed, out, y);
     cub::SyncStream(cudaStreamPerThread);
   } else {
-    omp_set_num_threads(std::thread::hardware_concurrency());
+    omp_set_num_threads(std::thread::hardware_concurrency() * 2);
     Impl(thrust::omp::par, m, n, sparsity, seed, out, y);
   }
   return 0;
