96c3170388c006cafdb5e2e9a8bcbe84e4e68270
Jiaming Yuan <jm.yuan@outlook.com>
2025-04-27 19:02:39 +0800

    Cleanup iter.

--- dxgb_bench/dataiter.py
+++ dxgb_bench/dataiter.py
@@ -5,13 +5,10 @@ import gc
 import os
 import re
 from abc import abstractmethod, abstractproperty
-from bisect import bisect_right
-from collections import defaultdict
 from typing import TYPE_CHECKING, Any, Callable, SupportsFloat, TypeAlias
 
 import numpy as np
 import xgboost as xgb
-from numpy import typing as npt
 from scipy import sparse
 from typing_extensions import override
 from xgboost.compat import concat
@@ -63,7 +60,7 @@ def load_Xy(
         X = np.load(Xp)
         y = np.load(yp)
     else:
-        assert Xp.endswith("kvi"), Xp
+        assert Xp.endswith("kio"), Xp
         import kvikio
 
         pinfo = get_pinfo(Xp)
@@ -102,28 +99,6 @@ def get_file_paths_local(dirname: str) -> tuple[list[str], list[str]]:
 fname_pattern = r"([X|y])_(\d+)_(\d+)-(\d+)-(\d+).[npy|npz|kvi]"
 
 
-def get_file_paths(loadfrom: list[str]) -> tuple[list[str], list[str]]:
-    X_files: list[str] = []
-    y_files: list[str] = []
-    for d in loadfrom:
-        X_fd, y_fd = get_file_paths_local(d)
-        X_files.extend(X_fd)
-        y_files.extend(y_fd)
-
-    def key(name: str) -> tuple[int, int]:
-        name = os.path.basename(name)
-        mat = re.search(fname_pattern, name)
-        assert mat is not None, name
-        batch = mat.group(4)
-        shard = mat.group(5)
-        return int(batch), int(shard)
-
-    X_files = sorted(X_files, key=key)
-    y_files = sorted(y_files, key=key)
-
-    return X_files, y_files
-
-
 def load_batches(
     loadfrom: list[str], device: str
 ) -> tuple[list[np.ndarray] | list[sparse.csr_matrix], list[np.ndarray]]:
@@ -158,29 +133,9 @@ def load_all(loadfrom: list[str], device: str) -> XyPair:
 def get_valid_sizes(
     n_samples: int, test_size: SupportsFloat = TEST_SIZE
 ) -> tuple[int, int]:
-    n_valid = int(n_samples * test_size)
+    n_valid = int(n_samples * float(test_size))
     n_train = n_samples - n_valid
     return n_train, n_valid
-
-
-def find_shard_ids(
-    indptr: npt.NDArray[np.int64], fold: int
-) -> tuple[int, int, int, int]:
-    n_samples = indptr[-1]
-    n_train, n_valid = get_valid_sizes(n_samples)
-
-    begin = n_valid * fold
-    end = begin + n_valid
-
-    assert end < n_samples
-
-    beg_idx: int = bisect_right(indptr, begin) - 1
-    end_idx: int = bisect_right(indptr, end) - 1
-
-    beg_in_shard = begin - indptr[beg_idx]
-    end_in_shard = end - indptr[end_idx]
-
-    return beg_idx, beg_in_shard, end_idx, end_in_shard
 
 
 def _alloc(
@@ -195,272 +150,6 @@ def _alloc(
         X = cp.empty(shape=(n_samples, n_features), dtype=np.float32)
         y = cp.empty(shape=n_samples, dtype=np.float32)
     return X, y
-
-
-def finish_futures(
-    futures: list[tuple[kvikio.IOFuture, kvikio.IOFuture]],
-) -> None:
-    for X_fut, y_fut in futures:
-        X_fut.get()
-        y_fut.get()
-
-
-def close_fds(fds: list[tuple[kvikio.CuFile, kvikio.CuFile]]) -> None:
-    for X_fd, y_fd in fds:
-        X_fd.close()
-        y_fd.close()
-
-
-class LoadIterImpl(IterImpl):
-    """Iterator for loading files."""
-
-    def __init__(
-        self, files: list[tuple[str, str]], split: bool, is_valid: bool, device: str
-    ) -> None:
-        assert files
-
-        X_shards = defaultdict(list)
-        y_shards = defaultdict(list)
-        for fname in files:
-            pinfo = get_pinfo(fname[0])
-            X_shards[pinfo.batch_idx].append(fname[0])
-            y_shards[pinfo.batch_idx].append(fname[1])
-            assert os.path.basename(fname[1]).startswith("y"), fname[1]
-
-        def key(name: str) -> int:
-            name = os.path.basename(name)
-            mat = re.search(fname_pattern, name)
-            assert mat is not None, name
-            shard = mat.group(5)
-            return int(shard)
-
-        X_shards_sorted = {}
-        for batch_idx, shards in X_shards.items():
-            shards = sorted(shards, key=key)
-            X_shards_sorted[batch_idx] = shards
-
-        y_shards_sorted = {}
-        for batch_idx, shards in y_shards.items():
-            shards = sorted(shards, key=key)
-            y_shards_sorted[batch_idx] = shards
-
-        self.X_shards: dict[int, list[str]] = X_shards_sorted
-        self.y_shards: dict[int, list[str]] = y_shards_sorted
-
-        assert len(self.X_shards) == len(self.y_shards)
-
-        self.split = split
-        self.is_valid = is_valid
-        self.device = device
-
-    def get_shard_indptr(self, i: int) -> npt.NDArray[np.int64]:
-        X_shards_i = self.X_shards[i]
-        y_shards_i = self.y_shards[i]
-
-        shard_sizes = [0]
-        for Xs, ys in zip(X_shards_i, y_shards_i):
-            pinfo_i = get_pinfo(Xs)
-            shard_sizes.append(pinfo_i.n_samples)
-            assert pinfo_i.batch_idx == i
-        indptr = np.cumsum(shard_sizes)
-        return indptr
-
-    def get_with_valid(self, i: int) -> tuple[np.ndarray, np.ndarray]:
-        """Function to read the data for training and validation."""
-        X_shards_i = self.X_shards[i]
-        y_shards_i = self.y_shards[i]
-
-        indptr = self.get_shard_indptr(i)
-        n_samples = indptr[-1]
-
-        n_train, n_valid = get_valid_sizes(n_samples)
-        # Span of the validation set.
-        beg_idx, beg_in_shard, end_idx, end_in_shard = find_shard_ids(indptr, 0)
-        pinfo = get_pinfo(X_shards_i[0])
-
-        if self.is_valid:
-            X, y = _alloc(n_valid, pinfo.n_features, self.device)
-        else:
-            X, y = _alloc(n_train, pinfo.n_features, self.device)
-
-        def read_xy_shard(
-            begin: int,
-            Xs: str,
-            ys: str,
-            prev: int,
-            out_end: int,
-            X: npt.NDArray[np.float32],
-            y: npt.NDArray[np.float32],
-        ) -> tuple[kvikio.IOFuture, kvikio.CuFile, kvikio.IOFuture, kvikio.CuFile]:
-            X_fd = kvikio.CuFile(Xs, "r")
-            offset_bytes = int(begin * pinfo.n_features * X.itemsize)
-            X_fut = X_fd.pread(
-                X[prev:out_end], X[prev:out_end].nbytes, file_offset=offset_bytes
-            )
-
-            y_fd = kvikio.CuFile(ys, "r")
-            offset_bytes = int(begin * y.itemsize)
-            y_fut = y_fd.pread(
-                y[prev:out_end], y[prev:out_end].nbytes, file_offset=offset_bytes
-            )
-            return X_fut, X_fd, y_fut, y_fd
-
-        prev = 0
-        fds = []
-        futures = []
-
-        def read_fst_part(Xs: str, ys: str, beg_in_shard: int) -> int:
-            begin, end = 0, beg_in_shard
-            out_end = prev + end - begin
-            diff = out_end - prev
-            if diff > 0:
-                X_fut, X_fd, y_fut, y_fd = read_xy_shard(
-                    begin, Xs, ys, prev, out_end, X, y
-                )
-
-                fds.append((X_fd, y_fd))
-                futures.append((X_fut, y_fut))
-                return out_end - prev
-            return 0
-
-        def read_snd_part(Xs: str, ys: str, end_in_shard: int) -> int:
-            begin, end = end_in_shard, pinfo.n_samples
-            out_end = prev + end - begin
-            diff = out_end - prev
-            if diff > 0:
-                X_fut, X_fd, y_fut, y_fd = read_xy_shard(
-                    begin, Xs, ys, prev, out_end, X, y
-                )
-
-                fds.append((X_fd, y_fd))
-                futures.append((X_fut, y_fut))
-                return out_end - prev
-            return 0
-
-        # Read each shard
-        if self.is_valid:
-            for sidx in range(beg_idx, end_idx + 1):
-                Xs = X_shards_i[sidx]
-                ys = y_shards_i[sidx]
-                vpinfo = get_pinfo(Xs)
-                if vpinfo.batch_idx == beg_idx:
-                    begin = 0
-                else:
-                    begin = beg_in_shard
-
-                if vpinfo.batch_idx == end_idx - 1:
-                    end = end_in_shard
-                else:
-                    end = vpinfo.n_samples
-
-                out_end = prev + end - begin
-                X_fut, X_fd, y_fut, y_fd = read_xy_shard(
-                    begin, Xs, ys, prev, out_end, X, y
-                )
-
-                fds.append((X_fd, y_fd))
-                futures.append((X_fut, y_fut))
-                prev += out_end - prev
-            finish_futures(futures)
-            close_fds(fds)
-            return X, y
-
-        for sidx in range(len(X_shards_i)):
-            Xs = X_shards_i[sidx]
-            ys = y_shards_i[sidx]
-            pinfo_i = get_pinfo(Xs)
-            # _, n_samples_i, n_features, bidx, sidx = get_pinfo(Xs)
-
-            if sidx >= beg_idx and sidx <= end_idx:
-                # May or may not read the full shard
-                if beg_idx == end_idx:
-                    # need to split up the shard and read it twice
-                    # Read first half
-                    prev += read_fst_part(Xs, ys, beg_in_shard)
-                    # Read second half
-                    prev += read_snd_part(Xs, ys, end_in_shard)
-                    continue
-                # The validation data spans across more than one shards.
-                if sidx > beg_idx and sidx < end_idx:
-                    # read full shard
-                    begin, end = 0, pinfo_i.n_samples
-                    out_end = prev + end - begin
-                    X_fut, X_fd, y_fut, y_fd = read_xy_shard(
-                        begin, Xs, ys, prev, out_end, X, y
-                    )
-
-                    fds.append((X_fd, y_fd))
-                    futures.append((X_fut, y_fut))
-                    prev += out_end - prev
-                    continue
-                if sidx == beg_idx:
-                    assert sidx <= end_idx
-                    # Read first half
-                    prev += read_fst_part(Xs, ys, beg_in_shard)
-                    continue
-                if sidx == end_idx:
-                    assert sidx > beg_idx
-                    # Read second half
-                    prev += read_snd_part(Xs, ys, end_in_shard)
-                    continue
-            else:
-                # Read full shard
-                begin, end = 0, pinfo_i.n_samples
-                out_end = prev + end - begin
-                X_fut, X_fd, y_fut, y_fd = read_xy_shard(
-                    begin, Xs, ys, prev, out_end, X, y
-                )
-
-                fds.append((X_fd, y_fd))
-                futures.append((X_fut, y_fut))
-                prev += out_end - prev
-
-        finish_futures(futures)
-        close_fds(fds)
-
-        return X, y
-
-    @override
-    def get(self, i: int) -> tuple[np.ndarray, np.ndarray]:
-        X_path, y_path = self.X_shards[i], self.y_shards[i]
-
-        if self.split:
-            X, y = self.get_with_valid(i)
-        else:
-            indptr = self.get_shard_indptr(i)
-            n_samples = indptr[-1]
-            # _, n_samples_i, n_features, bidx, sidx = get_pinfo(X_path[0])
-            pinfo = get_pinfo(X_path[0])
-            X, y = _alloc(n_samples, pinfo.n_features, self.device)
-            futures = []
-            fds = []
-
-            prev = 0
-            for Xs, ys in zip(X_path, y_path):
-                # _, n_samples_i, n_features, bidx, sidx = get_pinfo(Xs)
-                pinfo = get_pinfo(Xs)
-
-                out_end = prev + pinfo.n_samples
-
-                X_fd = kvikio.CuFile(Xs, "r")
-                X_fut = X_fd.pread(X[prev:out_end], X[prev:out_end].nbytes)
-
-                y_fd = kvikio.CuFile(ys, "r")
-                y_fut = y_fd.pread(y[prev:out_end], y[prev:out_end].nbytes)
-
-                prev += pinfo.n_samples
-
-                futures.append((X_fut, y_fut))
-                fds.append((X_fd, y_fd))
-
-            finish_futures(futures)
-            close_fds(fds)
-
-        return X, y
-
-    @property
-    def n_batches(self) -> int:
-        return len(self.X_shards)
 
 
 class LoadIterStrip(IterImpl):
