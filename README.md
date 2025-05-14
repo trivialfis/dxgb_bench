8c2a7a2c36a1b5e517a98b3528986ddcb8f37170
Jiaming Yuan <jm.yuan@outlook.com>
2025-05-14 23:16:00 +0800

    Remove the old dask script. (#22)

--- README.md
+++ README.md
@@ -4,15 +4,35 @@ To use the data gen written C++, we need to build it using CMake first. One can 
 `dxgb-bench` to generate the data, or generate them on the fly with the external memory
 version of XGBoost.
 
+``` sh
+mkdir build && cd build
+cmake .. -DCMAKE_CUDA_ARCHITECTURS=89 -GNinja
+ninja
+cd ../
+pip install -e . --no-build-isolation --no-deps
+```
+
 For both the batched `datagen` and the data iterator, the output should be consistent for
 different number of batches and for different devices. For example, generating 2 batches
 with 1024 samples for each batch should produce the exact same result as generating a
 single batch with 2048 samples. When compiled with CUDA, CPU and GPU output should match
 each other.
 
+Examples
+--------
+
+Run datagen:
+``` sh
+dxgb-bench datagen --n_samples_per_batch=4194304 --n_batches=4 --n_features=512 --device=cpu --fmt=npy
+```
+
+Run external memory test with synthesized on the fly.
+``` sh
+dxgb-ext-bench --fly --n_samples_per_batch=2097152 --n_features=256 --n_batches=8 --device=cuda --task=ext-qdm --n_rounds=8 --verbosity=1 --mr=arena
+```
+
 Commands
 --------
 - dxgb-bench
 - dxgb-dist-bench
 - dxgb-ext-bench
-<!-- - dxgb-dask-bench -->
--- dxgb_bench/datasets/__init__.py
+++ dxgb_bench/datasets/__init__.py
@@ -1,36 +1,44 @@
 import argparse
-from typing import Tuple
+from typing import TYPE_CHECKING, Tuple
 
-from .airline import Airline
-from .covtype import Covtype
-from .dataset import DataSet
-from .epsilon import Epsilon
-from .higgs import Higgs
-from .mortgage import Mortgage
-from .taxi import Taxi
-from .year import YearPrediction
+if TYPE_CHECKING:
+    from .dataset import DataSet
 
 
-def factory(name: str, args: argparse.Namespace) -> Tuple[DataSet, str]:
+def factory(name: str, args: argparse.Namespace) -> Tuple["DataSet", str]:
     if name.startswith("mortgage"):
-        d: DataSet = Mortgage(args)
+        from .mortgage import Mortgage
+
+        d: "DataSet" = Mortgage(args)
         return d, d.task
     elif name == "taxi":
+        from .taxi import Taxi
+
         d = Taxi(args)
         return d, d.task
     elif name == "higgs":
+        from .higgs import Higgs
+
         d = Higgs(args)
         return d, d.task
     elif name == "year":
+        from .year import YearPrediction
+
         d = YearPrediction(args)
         return d, d.task
     elif name == "covtype":
+        from .covtype import Covtype
+
         d = Covtype(args)
         return d, d.task
     elif name == "airline":
+        from .airline import Airline
+
         d = Airline(args)
         return d, d.task
     elif name == "epsilon":
+        from .epsilon import Epsilon
+
         d = Epsilon(args)
         return d, d.task
     else:
