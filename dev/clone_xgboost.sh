fb5d068dab62665ea971265f1bd9b02f1f84dbac
Jiaming Yuan <jm.yuan@outlook.com>
2025-06-25 16:31:12 +0800

    install.

--- dev/clone_xgboost.sh
+++ dev/clone_xgboost.sh
@@ -2,8 +2,8 @@ INSTALL_XGBOOST=$1
 
 if [[ -n ${INSTALL_XGBOOST} ]]; then
     cd /ws
-    git clone --recursive  https://github.com/dmlc/xgboost.git
+    git clone --recursive  https://github.com/trivialfis/xgboost.git
     cd xgboost
-    git checkout 6234b615a51c67193f001ea698ebcce7edb9d764
+    git checkout ext-compr
     cd /
 fi
