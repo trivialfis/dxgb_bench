1f5cac53f5903c2ad34f27b4641b9085fcb0701b
Jiaming Yuan <jm.yuan@outlook.com>
2025-06-25 16:50:46 +0800

    install.

--- dev/build_dxgb_bench.sh
+++ dev/build_dxgb_bench.sh
@@ -11,4 +11,4 @@ cd build
 cmake ../dxgb_bench/dxgb_bench -DCMAKE_BUILD_TYPE=RelWithDebInfo -DDXGB_USE_CUDA=ON -DCMAKE_CUDA_ARCHITECTURES=all -GNinja
 ninja
 cd ../dxgb_bench
-pip install . --no-deps --no-build-isolation
+pip install -e . --no-deps --no-build-isolation
