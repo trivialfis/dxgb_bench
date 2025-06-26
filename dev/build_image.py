61f95f0efbecb292a232e5977b9b1c1fa79d2a28
Jiaming Yuan <jm.yuan@outlook.com>
2025-06-26 16:00:44 +0800

    More parameters for image build.

--- dev/build_image.py
+++ dev/build_image.py
@@ -13,13 +13,17 @@ def main(args: argparse.Namespace) -> None:
     else:
         shutil.copyfile("dxgb_bench/dev/Dockerfile.gpu", "Dockerfile")
     build_args = args.build_args
+    if args.tag is None:
+        tag = f"dxgb-bench-{args.target}-{args.arch}-{args.sm}:latest"
+    else:
+        tag = args.tag
     cmd = [
         "docker",
         "build",
         "--progress=plain",
         ".",
         "-t",
-        f"dxgb-bench-{args.target}-{args.arch}-{args.sm}:latest",
+        tag,
         "--build-arg",
         f"SM={args.sm}",
         "--build-arg",
@@ -27,6 +31,12 @@ def main(args: argparse.Namespace) -> None:
     ]
     if args.install_xgboost:
         cmd.extend(["--build-arg", "INSTALL_XGBOOST=1"])
+    else:
+        cmd.extend(["--build-arg", "INSTALL_XGBOOST=''"])
+    if args.xgboost_checkout is not None:
+        cmd.extend(["--build-arg", f"XGBOOST_CHECKOUT={args.xgboost_checkout}"])
+    else:
+        cmd.extend(["--build-arg", "XGBOOST_CHECKOUT=''"])
     if build_args is not None:
         build_args_lst: list[str] = build_args.split(";")
         for kv in build_args_lst:
@@ -58,6 +68,8 @@ Examples:
         required=False,
     )
     parser.add_argument("--install-xgboost", action="store_true")
+    parser.add_argument("--xgboost-checkout", default=None, type=str)
     parser.add_argument("--target", choices=["cpu", "gpu"], default="gpu")
+    parser.add_argument("--tag", type=str, default=None)
     args = parser.parse_args()
     main(args)
--- dev/clone_xgboost.sh
+++ dev/clone_xgboost.sh
@@ -1,9 +1,13 @@
 INSTALL_XGBOOST=$1
+XGBOOST_CHECKOUT=$2
 
 if [[ -n ${INSTALL_XGBOOST} ]]; then
     cd /ws
     git clone --recursive  https://github.com/dmlc/xgboost.git
     cd xgboost
-    git checkout 6234b615a51c67193f001ea698ebcce7edb9d764
+    if [[ -n ${XGBOOST_CHECKOUT} ]]; then
+	echo "Checkout"
+	git checkout ${XGBOOST_CHECKOUT}
+    fi
     cd /
 fi
