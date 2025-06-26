3fbeec5b77c5cb68940ad98e03c4fdfab7e35a6a
Jiaming Yuan <jm.yuan@outlook.com>
2025-06-26 16:09:24 +0800

    Fix.

--- dev/clone_xgboost.sh
+++ dev/clone_xgboost.sh
@@ -1,6 +1,8 @@
 INSTALL_XGBOOST=$1
 XGBOOST_CHECKOUT=$2
 
+echo "INSTALL_XGBOOST: ${INSTALL_XGBOOST}, XGBOOST_CHECKOUT: ${XGBOOST_CHECKOUT}"
+
 if [[ -n ${INSTALL_XGBOOST} ]]; then
     cd /ws
     git clone --recursive  https://github.com/dmlc/xgboost.git
