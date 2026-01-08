562862a49357acbfc5bd53b61b2a1d613c5fac48
Jiaming Yuan <jm.yuan@outlook.com>
2026-01-08 15:54:01 +0800

    Upgrade.

--- dev/install_ncu.sh
+++ dev/install_ncu.sh
@@ -2,5 +2,4 @@
 
 set -euox pipefail
 
-apt install nsight-compute-2025.3.1 -y
-# https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/x86_64/nsight-compute-2025.4.0_2025.4.0.12-1_amd64.deb
+apt install nsight-compute-2025.4.0 -y
