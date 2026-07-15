29c8a36f1747f99078e504d0c3dc7b8bfcb8da2c
Jiaming Yuan <jm.yuan@outlook.com>
2026-07-15 23:14:26 +0800

    Update cuda (#57)

--- dev/install_nsys.sh
+++ dev/install_nsys.sh
@@ -2,23 +2,23 @@
 
 set -euox pipefail
 
-DEB_ARM="nsight-systems-2025.5.1_2025.5.1.121-1_arm64.deb"
-DEB_X86="nsight-systems-2025.5.1_2025.5.1.121-1_amd64.deb"
-URL_ARM="https://developer.nvidia.com/downloads/assets/tools/secure/nsight-systems/2025_5/${DEB_ARM}"
-URL_X86="https://developer.nvidia.com/downloads/assets/tools/secure/nsight-systems/2025_5/${DEB_X86}"
+DEB_ARM="nsight-systems-2026.3.1_2026.3.1.157-1_arm64.deb"
+DEB_X86="nsight-systems-2026.3.1_2026.3.1.157-1_amd64.deb"
+URL_ARM="https://developer.nvidia.com/downloads/assets/tools/secure/nsight-systems/2026_3/${DEB_ARM}"
+URL_X86="https://developer.nvidia.com/downloads/assets/tools/secure/nsight-systems/2026_3/${DEB_X86}"
 
 arch=$1
 
 case $arch in
     aarch)
 	wget $URL_ARM
-	echo "9fd77ef3e990e2564edc25b32474935a86157bff5d58403648e7bd1a2d6f4e83  ./${DEB_ARM}" | shasum -a 256 --check
+	echo "a4b202203b79525a7f24fb3b0dd1f6a52766d72979e8371db0e40b51962d6694  ./${DEB_ARM}" | shasum -a 256 --check
 	apt install ./${DEB_ARM} -y
 	rm ./${DEB_ARM}
 	;;
     x86)
 	wget $URL_X86
-	echo "b49be4830a9f550ce2dcd7412a5b93527014e3a57014d90f4b37e6ba65909cbc  ./${DEB_X86}" | shasum -a 256 --check
+	echo "5ee19712bab10f3f1848493ffe808d1bf540b5c6bdf0e06ac9da867dab28935b  ./${DEB_X86}" | shasum -a 256 --check
 	apt install ./${DEB_X86} -y
 	rm ./${DEB_X86}
 	;;
